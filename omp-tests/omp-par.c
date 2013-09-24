795c8c42c0cf4ca9003fdc60e12ed6df300d29e2
Jeremy Kemp <jakemp@uh.edu>
2013-09-24 17:35:58 -0500

    added timings

--- omp-tests/omp-par.c
+++ omp-tests/omp-par.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
-
+    struct timespec requestStart, requestEnd;
+    clock_gettime(CLOCK_REALTIME, &requestStart);
 #pragma omp parallel
     {
         printf("Hello World\n");
     }
+    clock_gettime(CLOCK_REALTIME, &requestEnd);
+    printf("Parallel region took %d second, and %d nanoseconds\n", requestEnd.tv_sec - requestStart.tv_sec, 
+                                                                   requestEnd.tv_nsec - requestStart.tv_nsec);
+#pragma omp parallel
+    {
+        printf("second parallel region\n");
+    }
+    clock_gettime(CLOCK_REALTIME, &requestEnd);
+    printf("Total of both parallel regions took %d second, and %d nanoseconds\n", requestEnd.tv_sec - requestStart.tv_sec, 
+                                                                   requestEnd.tv_nsec - requestStart.tv_nsec);
     return 0;
 }
