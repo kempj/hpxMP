4cfc68444c25d8e21c654847f00e2aa0c1610b9e
Jeremy Kemp <jakemp@uh.edu>
2013-10-02 19:01:46 -0500

    removed the print statement in the parallel region

--- omp-tests/omp-par.c
+++ omp-tests/omp-par.c
@@ -6,14 +6,12 @@ int main() {
     clock_gettime(CLOCK_REALTIME, &requestStart);
 #pragma omp parallel
     {
-        printf("Hello World\n");
     }
     clock_gettime(CLOCK_REALTIME, &requestEnd);
     printf("Parallel region took %d second, and %d nanoseconds\n", requestEnd.tv_sec - requestStart.tv_sec, 
                                                                    requestEnd.tv_nsec - requestStart.tv_nsec);
 #pragma omp parallel
     {
-        printf("second parallel region\n");
     }
     clock_gettime(CLOCK_REALTIME, &requestEnd);
     printf("Total of both parallel regions took %d second, and %d nanoseconds\n", requestEnd.tv_sec - requestStart.tv_sec, 
