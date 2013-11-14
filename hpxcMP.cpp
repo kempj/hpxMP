6c8fda95e70b740e9982f736be8b8e9db511e8ec
Jeremy Kemp <jakemp@uh.edu>
2013-11-14 14:11:52 -0600

    integrating new code into makefiles

--- hpxcMP.cpp
+++ hpxcMP.cpp
@@ -1,26 +1,26 @@
 #include "hpxMP.h"
 #include <iostream>
 #include <cstdlib>
+#include <hpxc/threads.h>
 
 #include <hpx/hpx.hpp>
 #include <hpx/hpx_fwd.hpp>
 #include <hpx/hpx_init.hpp>
 #include <hpx/runtime/threads/topology.hpp>
 #include <hpx/lcos/local/barrier.hpp>
-#include <hpx/include/lcos.hpp>
 
+#include <atomic>
+ 
 using namespace std;
 using hpx::lcos::local::barrier;
-using hpx::lcos::future;
 
-vector<hpx::lcos::future<void>> threads;
-hpx::lcos::local::spinlock single_lock;
-
+hpxc_thread_t *threads;
+hpxc_mutex_t single_lock = HPXC_MUTEX_INITIALIZER;
 
 void (*omp_task)(int, void*)=0;
 int num_threads = 0;
 bool started = false;
-barrier *globalBarrier;
+barrier *b;
 int single_counter = 0;
 int current_single_thread = -1;
 
@@ -35,56 +35,66 @@ int init_num_threads() {
     return numThreads;
 }
 
-void thread_setup(void (*micro_task)(int, void*), int thread_num, void *fp) {
-    auto thread_id = hpx::threads::get_self_id();
-    hpx::threads::set_thread_data( thread_id, thread_num);
-    micro_task(thread_num, fp);
-}
-
+//There is a reference to a struct being passed as the second argument to the 
+//micro_taskthat seems to be accessed only on nested parallel regions. 
+//The first argument is the global_tid.
+//So, nested parallel regions should be disabled now; running in serial after the first fork
 int hpx_main() {
-    threads.reserve(num_threads);
-    globalBarrier = new barrier(num_threads);
+    threads = new hpxc_thread_t[num_threads];
+    b = new barrier(num_threads);
+//    cout << "hello from hpx main (" << num_threads << " threads)" << endl;
     for(int i = 0; i < num_threads; i++) {
-        threads.push_back( hpx::async(thread_setup, *omp_task, i, (void*)0));
+        hpxc_thread_create( &threads[i], 0, (void* (*)(void*))omp_task, 0);
     }
-    hpx::lcos::wait(threads);
+    for(int i = 0; i < num_threads; i++) {
+        hpxc_thread_join( threads[i], 0);
+    }
     return hpx::finalize();
 }
 
 void __ompc_fork(int Nthreads, omp_micro micro_task, frame_pointer_t fp) {
+    //struct timespec start_time, end_time;
     if(started) {
-        vector<future<void>> local_threads;
-        local_threads.reserve(num_threads);
+        hpxc_thread_t *local_threads = new hpxc_thread_t[num_threads];
         for(int i = 0; i < num_threads; i++) {
-            local_threads.push_back(hpx::async(*micro_task, i, fp));//should this be i or the current omp_thread_num
+            hpxc_thread_create( &local_threads[i], 0, (void* (*)(void*))micro_task, 0);
         }
-        hpx::lcos::wait(local_threads);
+        for(int i = 0; i < num_threads; i++) {
+            hpxc_thread_join(local_threads[i], 0);
+        }
     } else {
         started = true;
         omp_task = micro_task;
         num_threads = init_num_threads();
+        //clock_gettime(CLOCK_REALTIME, &start_time);
         hpx::init();
+        //clock_gettime(CLOCK_REALTIME, &end_time);
+        //cout << endl << end_time.tv_sec - start_time.tv_sec << ", "
+        //     << end_time.tv_nsec - start_time.tv_nsec << endl;
         started = false;
     }
 }
 
-int __ompc_can_fork() {
+omp_int32 __ompc_can_fork() {
     return !started;
 }
 
-int __ompc_get_local_thread_num() {
-    auto thread_id = hpx::threads::get_self_id();
-    int thread_num = hpx::threads::get_thread_data( thread_id );
-    return thread_num;
+omp_int32 __ompc_get_local_thread_num() {
+    for(int i = 0; i < num_threads; i++) {
+        if(hpxc_thread_equal(hpxc_thread_self(), threads[i])) {
+            return i;
+        }
+    }
+    return 0;
 }
 
-void __ompc_static_init_4( int global_tid, omp_sched_t schedtype,
-                           int *p_lower, int *p_upper, 
-                           int *p_stride, int incr, 
-                           int chunk) {
+void __ompc_static_init_4( omp_int32 global_tid, omp_sched_t schedtype,
+                           omp_int32 *p_lower, omp_int32 *p_upper, 
+                           omp_int32 *p_stride, omp_int32 incr, 
+                           omp_int32 chunk) {
     int thread_num = __ompc_get_local_thread_num();
     int size;
-    int *tmp;
+    omp_int32 *tmp;
     if(*p_upper < *p_lower) {
         tmp = p_upper;
         p_upper = p_lower;
@@ -106,44 +116,45 @@ void __ompc_barrier() {
 }
 
 void __ompc_ebarrier() {
-    globalBarrier->wait();
+    b->wait();
 }
 
-int __ompc_get_num_threads(){
+omp_int32 __ompc_get_num_threads(){
     return 0;
 }
 
-int __ompc_master(int global_tid){
+omp_int32 __ompc_master(omp_int32 global_tid){
     if(__ompc_get_local_thread_num() == 0) 
         return 1;
     return 0;
 }
 
-void __ompc_end_master(int global_tid){
+void __ompc_end_master(omp_int32 global_tid){
 }
 
-int __ompc_single(int global_tid){
+omp_int32 __ompc_single(omp_int32 global_tid){
     int tid = __ompc_get_local_thread_num();
-    single_lock.lock();
+    hpxc_mutex_lock(&single_lock);
     if(current_single_thread == -1 && single_counter == 0) {
         current_single_thread = tid;
         single_counter = 1 - num_threads;
     } else {
         single_counter++;
     }
-    single_lock.unlock();
+    hpxc_mutex_unlock(&single_lock);
     if(current_single_thread == tid) 
         return 1;
     return 0;
 }
 
-void __ompc_end_single(int global_tid){
-    single_lock.lock();
+void __ompc_end_single(omp_int32 global_tid){
+    hpxc_mutex_lock(&single_lock);
     if(single_counter == 0) {
         current_single_thread = -1;
     }
-    single_lock.unlock();
+    hpxc_mutex_unlock(&single_lock);
 }
+
 int __ompc_task_will_defer(int may_delay){
     //in the OpenUH runtime, this also checks if a task limit has been reached
     //leaving that to hpx to decide
@@ -155,12 +166,12 @@ void __ompc_task_firstprivates_alloc(void **firstprivates, int size){
 }
 
 void __ompc_task_firstprivates_free(void *firstprivates){
-    //Not sure if anything needs to be allocated yet.
+    //similar to alloc, not implementing yet
 }
 
 void __ompc_task_create( omp_task_func taskfunc, void *frame_pointer,
                          void *firstprivates, int may_delay,
-                         int is_tied, int blocks_parent) {
+                         int is_tied, int blocks_parent){
 }
 
 void __ompc_task_wait(){
@@ -170,25 +181,25 @@ void __ompc_task_exit(){
 }
 
 
-void __ompc_serialized_parallel(int global_tid) {
+void __ompc_serialized_parallel(omp_int32 global_tid) {
     //It appears this function does nothing
 }
-void __ompc_end_serialized_parallel(int global_tid) {
+void __ompc_end_serialized_parallel(omp_int32 global_tid) {
     //It appears this function does nothing
 }
 void __ompc_task_exit() {
     //It appears this function does nothing
 }
 
-int omp_get_num_threads() {
+omp_int32 omp_get_num_threads() {
     return num_threads;
 }
 
-int omp_get_max_threads() {
+omp_int32 omp_get_max_threads() {
     return num_threads;
 }
 
-int omp_get_thread_num() {
+omp_int32 omp_get_thread_num() {
     return __ompc_get_local_thread_num();
 }
 
