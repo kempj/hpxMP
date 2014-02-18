e99ad76d2aaa325b7152662a69119f1c68363a2c
Jeremy Kemp <jakemp@uh.edu>
2014-02-18 16:57:48 -0600

    minor fix to omp barrier

--- hpxMP.cpp
+++ hpxMP.cpp
@@ -150,6 +150,10 @@ void __ompc_barrier() {
 }
 
 void __ompc_ebarrier() {
+    //This is added because a barrier is supposed to wait for all current tasks to finish.
+    //In the case where tasks were spawned, but taskwait was not called, this is needed
+    hpx_backend->task_wait();
+
     hpx_backend->barrier_wait();
 }
 
