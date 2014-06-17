776c5f165843609ecba5274bf047394d9b431ace
Jeremy Kemp <jakemp@uh.edu>
2014-06-17 14:34:18 -0500

    added hpx simple fib

---
+++ hpx-tests/fibonacci.cpp
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+// Naive SMP version implemented with futures.
+
+#include <hpx/hpx.hpp>
+#include <hpx/hpx_init.hpp>
+//#include <hpx/include/actions.hpp>
+#include <hpx/include/util.hpp>
+#include <iostream>
+#include <boost/cstdint.hpp>
+#include <boost/format.hpp>
+
+
+boost::uint64_t fibonacci(boost::uint64_t n) {
+    if (n < 2)
+        return n;
+    hpx::future<boost::uint64_t> n1 = hpx::async(fibonacci, n - 1);
+    hpx::future<boost::uint64_t> n2 = hpx::async(fibonacci, n - 2);
+
+    return n1.get() + n2.get();
+}
+
+int hpx_main(boost::program_options::variables_map& vm)
+{
+    boost::uint64_t n = vm["n-value"].as<boost::uint64_t>();
+    {
+        hpx::util::high_resolution_timer t;
+        boost::uint64_t r = fibonacci(n);
+        char const* fmt = "fibonacci(%1%) == %2%\nelapsed time: %3% [s]\n";
+        std::cout << (boost::format(fmt) % n % r % t.elapsed());
+    }
+    return hpx::finalize(); // Handles HPX shutdown
+}
+
+int main(int argc, char* argv[])
+{
+    boost::program_options::options_description
+       desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
+
+    desc_commandline.add_options()
+        ( "n-value",
+          boost::program_options::value<boost::uint64_t>()->default_value(10),
+          "n value for the Fibonacci function");
+
+    return hpx::init(desc_commandline, argc, argv);
+}
