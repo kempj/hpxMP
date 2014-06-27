f606ce39181451c4fe659558728ee366a271684f
Jeremy Kemp <jakemp@uh.edu>
2014-06-27 16:19:48 -0500

    finished fib1_2 conversion

--- omp-tests/omp-fib.w2c.c
+++ omp-tests/omp-fib.w2c.c
@@ -1,6 +1,6 @@
 #include <sys/time.h>
 #include <stdio.h>
-#include "hpxMP.h"
+#include "../hpxMP.h"
 
 
 struct task_args_fib {
@@ -46,7 +46,7 @@ int main(int argc, char ** argv)
     __ompc_fork(0, &parallel_region_func, &fake_fp);
 
     gettimeofday(&t2, (struct timezone *) 0ULL);
-    printf("fib(%d)= %d\n", input, fake_fp.result);
+    printf("fib(%ld)= %d\n", input, fake_fp.result);
     s = (t2).tv_sec - (t1).tv_sec;
     u = (t2).tv_usec - (t1).tv_usec;
     m = ((double)((s * 1000LL)) + ((double)(u) / 1.0e+03)) + 5.0e-01;
@@ -114,59 +114,28 @@ long fib1(int k)
 static void fib1_1(struct task_args_fib *taskargs, void *fake_fp)
 {
     long_int_struct *fp = (long_int_struct *)fake_fp;
-    //struct task_args_fib tmp_k;
     int local_k;
 
-    if(taskargs)
-    {
-        local_k = (taskargs)->k;
-//        local_k = *(int *) & tmp_k;
-//    } else {
-//        local_k = *((int *) _temp___slink_sym8 + -4LL);
-//    }
-    fp->result = fib1(local_k + -2);
+    assert(taskargs);
+
+    local_k = (taskargs)->k;
+    fp->result = fib1(local_k - 2);
     __ompc_task_exit();
-    if(taskargs) {
-        __ompc_task_firstprivates_free(taskargs);
-    }
+    __ompc_task_firstprivates_free(taskargs);
     return;
 }
 
-
 static void fib1_2(task_args_fib *taskargs, void *fake_fp)
 {
-
-    register _UINT64 _w2c_reg6;
-    register _UINT64 _w2c_reg5;
-    struct task_args_fib _tmp0;
-    register long _w2c___comma;
-    register int _w2c_reg1;
-    _UINT64 _temp___slink_sym9;
+    long_int_struct *fp = (long_int_struct *)fake_fp;
     int local_k;
 
-    _temp___slink_sym9 = _w2c_reg6;
-    taskargs = (struct task_args_fib *)(_w2c_reg5);
-    fake_fp = _w2c_reg6;
-    _temp___slink_sym9 = fake_fp;
-    if(!(taskargs))
-        goto _1539;
-    _tmp0 = (struct task_args_fib)(taskargs) -> k;
-    local_k = *(int *) & _tmp0;
-    goto _1283;
-_1539 :;
-       local_k = *((int *) _temp___slink_sym9 + -4LL);
-_1283 :;
-       _w2c_reg5 = local_k + -1;
-       _w2c___comma = fib1(local_k + -1);
-       * ((long *) _temp___slink_sym9 + -9LL) = _w2c___comma;
-       _w2c_reg1 = 0;
-       __ompc_task_exit();
-       if(!(taskargs))
-           goto _1795;
-       _w2c_reg5 = (_UINT64)(taskargs);
-       _w2c_reg1 = 0;
-       __ompc_task_firstprivates_free(taskargs);
-_1795 :;
-       return;
+    assert(taskargs);
+
+    local_k = (taskargs)->k;
+    fp->result = fib1(local_k - 1);
+    __ompc_task_exit();
+    __ompc_task_firstprivates_free(taskargs);
+    return;
 }
 
