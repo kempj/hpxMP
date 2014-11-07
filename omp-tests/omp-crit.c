bdb577dd8bf71b949b51d8684aa4a77e0e39eac4
Jeremy Kemp <jakemp@uh.edu>
2014-11-06 22:48:13 -0600

    added crit test

---
+++ omp-tests/omp-crit.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int x = 0;
+
+#pragma omp parallel
+    {
+        x = 3;//omp_get_thread_num();
+        printf("x = %d\n",x);
+#pragma omp critical
+        x = 42;
+        printf("\tx = %d\n",x);
+    }
+    return 0;
+}
