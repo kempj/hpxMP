9e4b93a005d9e0d0a3797d4ab4f855173a4b22f9
Jeremy Kemp <jakemp@uh.edu>
2015-01-11 15:30:54 -0600

    improved output and formatting of omp tests

--- omp/tests/runtests.sh
+++ omp/tests/runtests.sh
@@ -5,15 +5,21 @@ TEST_DIR=bin/$RT/$CC
 logfile=logs/$CC-$RT.log
 
 echo "running the $CC compiled code with the $RT runtime"
-echo $logfile
+echo "output written to $logfile"
+
+echo `date` > $logfile
 
 for test in $TEST_DIR/*
 do
-    echo "running $test"
+    echo "running $test" | tee -a $logfile
     LD_PRELOAD=../../$RT timeout 30s ./$test >> $logfile
-    if [ $? -eq 124 ]
+    retval=$?
+    if [ $retval -eq 124 ]
     then 
-        echo "ERROR: test ran for longer than 30 seconds"
+        echo "ERROR: test ran for longer than 30 seconds" | tee -a $logfile
+    elif [ $retval -ne 0 ]
+    then echo "Error: test returned ($retval)" | tee -a $logfile
     fi
+
 done
 
