0c457e177443252ed7798584f38161482a3aed4e
Jeremy Kemp <jakemp@uh.edu>
2015-01-15 14:06:49 -0600

    added a test number

--- omp/tests/runtests.sh
+++ omp/tests/runtests.sh
@@ -11,17 +11,19 @@ echo "output written to $logfile"
 
 echo `date` > $logfile
 
+test_num=0
+
 for test in $TEST_DIR/*
 do
-    echo "running $test" | tee -a $logfile
+    echo "running test #$test_num: $test" | tee -a $logfile
     OMP_NUM_THREADS=$nthreads LD_PRELOAD=$preload timeout 30s ./$test >>$logfile 2>&1
     retval=$?
     if [ $retval -eq 124 ]
     then 
-        echo "ERROR: test ran for longer than 30 seconds" | tee -a $logfile
+        echo "\tERROR: test ran for longer than 30 seconds" | tee -a $logfile
     elif [ $retval -ne 0 ]
-    then echo "Error: test returned ($retval)" | tee -a $logfile
+    then echo "\tError: test returned ($retval)" | tee -a $logfile
     fi
-
+    test_num=$((test_num+1))
 done
 
