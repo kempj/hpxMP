1bc4afb180d76a36120bb1f3312c4ab06120ac72
Jeremy Kemp <jakemp@uh.edu>
2015-01-30 19:04:33 -0600

    working on loops

--- loop_schedule.cpp
+++ loop_schedule.cpp
@@ -20,6 +20,15 @@ void omp_static_init( int gtid, int schedtype, int *p_last_iter,
     int trip_count = (*p_upper - *p_lower) / incr + 1;
     int adjustment = ((trip_count % team_size) == 0) ? -1 : 0;
 
+
+    loop_sched->lock();
+    cout << "beginning of static_init " << endl;
+    cout << "thread " << gtid << " out of " << team_size << ": " << endl;
+    cout << "lower = " << *p_lower << ", upper = " << *p_upper << ", last = " 
+         << *p_last_iter << ", p_stride  = " << *p_stride << endl;
+    cout << "incr = " << incr << ", chunk = " << chunk << endl;
+    loop_sched->unlock();
+
     if(team_size == 1) {
         *p_last_iter = 1;
         return;
