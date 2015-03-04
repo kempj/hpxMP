affca926b6a127c6ea219cdcfe4dfa415d3ca1c5
Jeremy Kemp <jakemp@uh.edu>
2015-03-04 16:09:34 -0600

    normalized for inner reps like epcc does

--- hpx/bench/hpx-taskbench.cpp
+++ hpx/bench/hpx-taskbench.cpp
@@ -19,20 +19,6 @@ using std::endl;
 const int DEPTH = 6;
 int delay_length;
 
-void print_time(std::vector<uint64_t> time, std::string name) {
-    uint64_t total = 0, min = time[0], max = 0;
-    for(int i = 0; i < time.size(); i++) {
-        total += time[i];
-        if(time[i] > max) {
-            max = time[i];
-        }
-        if(time[i] < min) {
-            min = time[i];
-        }
-    }
-    cout << endl << name << ", (average, min, max) in ns:" << endl
-         << (total / time.size()) << ", " << min << ", " << max << endl;
-}
 
 void delay(int delaylength) {
     float a = 0.;
@@ -40,24 +26,6 @@ void delay(int delaylength) {
         a += i;
     if(a < 0)
         printf("%f \n", a);
-}
-
-int getdelaylengthfromtime(uint64_t delaytime) {
-    int delaylength = 0;
-    int reps = 1000;
-    uint64_t starttime, lapsedtime = 0;
-    delaytime = delaytime/1.0E6; // convert from microseconds to seconds
-    delay(delaylength);
-
-    while(lapsedtime < delaytime) {
-        delaylength = delaylength * 1.1 + 1;
-        starttime = hpx::util::high_resolution_clock::now();
-        for(int i = 0; i < reps; i++) {
-            delay(delaylength);
-        }
-        lapsedtime = (hpx::util::high_resolution_clock::now() - starttime) / (double) reps;
-    }
-    return delaylength;
 }
 
 //PARALLEL TASK
@@ -233,6 +201,21 @@ uint64_t testLeafTaskGeneration(int num_threads, int inner_reps) {
     return hpx::util::high_resolution_clock::now() - start;
 }
 
+void print_time(std::vector<double> time, std::string name) {
+    double total = 0, min = time[0], max = 0;
+    for(int i = 0; i < time.size(); i++) {
+        total += time[i];
+        if(time[i] > max) {
+            max = time[i];
+        }
+        if(time[i] < min) {
+            min = time[i];
+        }
+    }
+    cout << endl << name << ", (average, min, max) in ns:" << endl
+         << (total / time.size()) << ", " << min << ", " << max << endl;
+}
+
 int hpx_main(boost::program_options::variables_map& vm) {
     std::string test = vm["test"].as<std::string>();
     int reps = vm["reps"].as<int>();
@@ -240,7 +223,7 @@ int hpx_main(boost::program_options::variables_map& vm) {
     int delay_time = vm["delay_time"].as<int>();
     int inner_reps = 20;
     int num_threads = hpx::get_os_thread_count();
-    vector<uint64_t> time(reps);
+    vector<double> time(reps);
     //delay_length = getdelaylengthfromtime(delay_time);
     delay_length = 490;
 
@@ -249,12 +232,12 @@ int hpx_main(boost::program_options::variables_map& vm) {
     }
     
     for(int i = 0; i < reps; i++) {
-        time[i] = testParallelTaskGeneration(num_threads, inner_reps);
+        time[i] = testParallelTaskGeneration(num_threads, inner_reps) / inner_reps;
     }
     print_time(time, "PARALLEL TASK");//20
 
     for(int i = 0; i < reps; i++) {
-        time[i] = testMasterTaskGeneration(num_threads, inner_reps);
+        time[i] = testMasterTaskGeneration(num_threads, inner_reps) / inner_reps;
     }
     print_time(time, "MASTER TASK");//20
 
@@ -267,7 +250,7 @@ int hpx_main(boost::program_options::variables_map& vm) {
         inner_reps *= timing_version;
     }
     for(int i = 0; i < reps; i++) {
-        time[i] = testMasterTaskGenerationWithBusySlaves(num_threads, inner_reps);
+        time[i] = testMasterTaskGenerationWithBusySlaves(num_threads, inner_reps) / inner_reps;
     }
     print_time(time, "MASTER TASK BUSY SLAVES");//1280 / 640 (hpxMP)
 
@@ -280,12 +263,12 @@ int hpx_main(boost::program_options::variables_map& vm) {
         inner_reps *= timing_version;
     }
     for(int i = 0; i < reps; i++) {
-        time[i] = testTaskWait(num_threads, inner_reps);
+        time[i] = testTaskWait(num_threads, inner_reps) / inner_reps;
     }
     print_time(time, "TASK WAIT");//1280 / 80 (hpxMP)
 
     for(int i = 0; i < reps; i++) {
-        time[i] = testNestedTaskGeneration(num_threads, inner_reps);
+        time[i] = testNestedTaskGeneration(num_threads, inner_reps) / inner_reps;
     }
     print_time(time, "NESTED TASK");//2560 / 80 (hpxMP)
 
@@ -298,17 +281,17 @@ int hpx_main(boost::program_options::variables_map& vm) {
         inner_reps *= timing_version;
     }
     for(int i = 0; i < reps; i++) {
-        time[i] = testNestedMasterTaskGeneration(num_threads, inner_reps);
+        time[i] = testNestedMasterTaskGeneration(num_threads, inner_reps) / inner_reps;
     }
     print_time(time, "NESTED MASTER TASK");//2560 / 160 (hpxMP)
 
     for(int i = 0; i < reps; i++) {
-        time[i] = testBranchTaskGeneration(num_threads, inner_reps);
+        time[i] = testBranchTaskGeneration(num_threads, inner_reps) / inner_reps;
     }
     print_time(time, "BRANCH TASK TREE");//5120 / 160 (hpxMP)
 
     for(int i = 0; i < reps; i++) {
-        time[i] = testLeafTaskGeneration(num_threads, inner_reps);
+        time[i] = testLeafTaskGeneration(num_threads, inner_reps) / inner_reps;
     }
     print_time(time, "LEAF TASK TREE");//5120 / 160 (hpxMP)
 
@@ -339,3 +322,4 @@ int main(int argc, char ** argv) {
 
     return hpx::init(desc_commandline, argc, argv, cfg);
 }
+
