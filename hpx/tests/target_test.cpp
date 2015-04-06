c59f98cbea5f8671b0eb4116d8bdab855a560fc2
Jeremy Kemp <jakemp@uh.edu>
2015-04-06 18:55:41 -0500

    got a simple distributed hpx program to work.

--- hpx/tests/target_test.cpp
+++ hpx/tests/target_test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <hpx/hpx.hpp>
-
+/*
 void foo(int *in_vals, int size, int *out_vals)
 {
     for(int i = 0; i < size; i++) {
@@ -8,13 +8,24 @@ void foo(int *in_vals, int size, int *out_vals)
     }
 }
 
+HPX_PLAIN_ACTION(foo, foo_action);
+*/
+int add(int val1, int val2)
+{
+    return val1 + val2;
+}
+HPX_PLAIN_ACTION(add, add_action);
+
+
 int hpx_main()
 {
+    /*
     const int size = 10;
     int in[size], out[size];
     for(int i = 0; i < size; i++) {
         in[i] = i;
     }
+
 
     foo(in, size, out);
 
@@ -22,6 +33,14 @@ int hpx_main()
         std::cout << out[i] << ", ";
     }
     std::cout << std::endl;
+    */
+
+    auto localities = hpx::find_all_localities();
+    
+    for( auto node : localities ) {
+        auto result = hpx::async(add_action(), node, 2, 3);
+        std::cout << result.get() << std::endl;
+    }
 
     return hpx::finalize();
 }
