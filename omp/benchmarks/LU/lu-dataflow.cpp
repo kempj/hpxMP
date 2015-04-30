05de7ac5d938dd09523f2d871489f528de750c68
Jeremy Kemp <jakemp@uh.edu>
2015-04-30 18:52:52 -0500

    moving towards depend clause

--- omp/benchmarks/LU/lu-dataflow.cpp
+++ omp/benchmarks/LU/lu-dataflow.cpp
@@ -8,58 +8,58 @@ using std::vector;
 vector<double> A;
 
 
-void init_df(vector<vector<vector<block>>> &blockList, int numBlocks, int size) {
+void init_df(vector<vector<vector<block>>> &block_list, int numBlocks, int size) {
     
-    getBlockList(blockList[0], numBlocks, size);
-    blockList[1].resize(numBlocks);
+    getBlockList(block_list[0], numBlocks, size);
+    block_list[1].resize(numBlocks);
 
     for(int i = 0; i < numBlocks; i++){
-        blockList[1][i].resize( numBlocks );
+        block_list[1][i].resize( numBlocks );
     }
 
-#pragma omp task
-    blockList[0][0][0] = diag_op( size, blockList[0][0][0] );
+#pragma omp task shared(block_list) depend( in: block_list[0][0][0] )
+    block_list[0][0][0] = diag_op( size, block_list[0][0][0] );
     for(int i = 1; i < numBlocks; i++) {
-#pragma omp task
-        blockList[0][0][i] = row_op( size, blockList[0][0][i], 
-                                           blockList[0][0][0] );
+#pragma omp task shared(block_list)
+        block_list[0][0][i] = row_op( size, block_list[0][0][i], 
+                                           block_list[0][0][0] );
     }
     for(int i = 1; i < numBlocks; i++) {
-#pragma omp task
-        blockList[0][i][0] = col_op( size, blockList[0][i][0], 
-                                           blockList[0][0][0] );
+#pragma omp task shared(block_list)
+        block_list[0][i][0] = col_op( size, block_list[0][i][0], 
+                                           block_list[0][0][0] );
         for(int j = 1; j < numBlocks; j++) {
-#pragma omp task
-            blockList[0][i][j] = inner_op( size, blockList[0][i][j],
-                                                 blockList[0][0][j],
-                                                 blockList[0][i][0] );
+#pragma omp task shared(block_list)
+            block_list[0][i][j] = inner_op( size, block_list[0][i][j],
+                                                 block_list[0][0][j],
+                                                 block_list[0][i][0] );
         }
     }
 }
 
 void LU( int size, int numBlocks)
 {
-    vector<vector<vector<block>>> blockList(2);
+    vector<vector<vector<block>>> block_list(2);
 
-    init_df(blockList, numBlocks, size);
+    init_df(block_list, numBlocks, size);
 
     for(int i = 1; i < numBlocks; i++) {
 #pragma omp task
-        blockList[i%2][i][i] = diag_op( size, blockList[(i-1)%2][i][i] );
+        block_list[i%2][i][i] = diag_op( size, block_list[(i-1)%2][i][i] );
         for(int j = i + 1; j < numBlocks; j++){
 #pragma omp task
-            blockList[i%2][i][j] = row_op( size, blockList[(i-1)%2][i][j],
-                                                 blockList[ i   %2][i][i] );
+            block_list[i%2][i][j] = row_op( size, block_list[(i-1)%2][i][j],
+                                                 block_list[ i   %2][i][i] );
         }
         for(int j = i + 1; j < numBlocks; j++){
 #pragma omp task
-            blockList[i%2][j][i] = col_op( size, blockList[(i-1)%2][j][i], 
-                                                 blockList[ i   %2][i][i] );
+            block_list[i%2][j][i] = col_op( size, block_list[(i-1)%2][j][i], 
+                                                 block_list[ i   %2][i][i] );
             for(int k = i + 1; k < numBlocks; k++) {
 #pragma omp task
-                blockList[i%2][j][k] = inner_op( size, blockList[(i-1)%2][j][k], 
-                                                       blockList[ i   %2][i][k],
-                                                       blockList[ i   %2][j][i] );
+                block_list[i%2][j][k] = inner_op( size, block_list[(i-1)%2][j][k], 
+                                                       block_list[ i   %2][i][k],
+                                                       block_list[ i   %2][j][i] );
             }
         }
     }
