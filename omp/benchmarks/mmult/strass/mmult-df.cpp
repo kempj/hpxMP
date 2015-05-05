bd5e426b6e8011a9e3ad0a4866a3c4c8b36b443d
Jeremy Kemp <jakemp@uh.edu>
2015-05-05 15:52:51 -0500

    another small fix for mmult taskwait

--- omp/benchmarks/mmult/strass/mmult-df.cpp
+++ omp/benchmarks/mmult/strass/mmult-df.cpp
@@ -94,11 +94,9 @@ int main(int argc, char **argv)
     int numBlocks = size/blocksize;
     auto t1 = high_resolution_clock::now();
     mmult(numBlocks, size, &R1, blR1, &A, blA, &B, blB);
-#pragma omp taskwait
     
     auto t2 = high_resolution_clock::now();
     mmult(numBlocks, size, &R2, blR2, &R1, blR1, &C, blC);
-#pragma omp taskwait
 
     auto t3 = high_resolution_clock::now();
     mmult(numBlocks, size, &R3, blR3, &R1, blR1, &R2, blR2);
