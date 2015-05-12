f41930c6d784996ce5c06654cff46d0cc7397ea4
Jeremy Kemp <jakemp@uh.edu>
2015-05-12 18:27:55 -0500

    fixing overflow

--- omp/benchmarks/tasks/task-spawn.cpp
+++ omp/benchmarks/tasks/task-spawn.cpp
@@ -96,8 +96,9 @@ int main(int argc, char ** argv) {
     
     auto dep_total = duration_cast<nanoseconds> (t2-t1).count();
 
-    int64_t theory = num_tasks * delay_time;
+    int64_t theory = num_tasks;
     theory /= num_threads; 
+    theory *= delay_time;
     cout << "theory time = " << theory    / 1000000.0 << " ms (" << num_tasks * delay_time << " * " << num_threads << " threads)" << endl;
     cout << "spawn time  = " << total     / 1000000.0 << " ms : " << (    total - theory) / 1000000.0 << " ms overhead " << endl;
     cout << "depend time = " << dep_total / 1000000.0 << " ms : " << (dep_total - theory) / 1000000.0 << " ms overhead " << endl;
