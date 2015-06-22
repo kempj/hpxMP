9cf20371ba81cd063ca77a765f75ede00a342660
Jeremy Kemp <jakemp@uh.edu>
2015-06-22 13:08:39 -0500

    minor test change

--- hpx/tests/omp_queue_test.cpp
+++ hpx/tests/omp_queue_test.cpp
@@ -1,20 +1,27 @@
 #include <hpx/hpx_init.hpp>
 #include <hpx/hpx.hpp>
 #include <unistd.h>
+#include <iostream>
 
 using hpx::lcos::shared_future;
 
 void delay(int seconds) {
+    std::cout << "\tdelay" << std::endl;
     sleep(seconds);
+    std::cout << "\tend delay" << std::endl;
 }
 
 void wait_delay(int seconds, shared_future<void> f1){//, future<void> f2, future<void> f3) {
+    std::cout << "\twait and delay" << std::endl;
     sleep(seconds);
     hpx::async(delay, 2);
+    std::cout << "\twaiting" << std::endl;
     f1.wait();
+    std::cout << "\tdone waiting" << std::endl;
     //f2.wait();
     //f3.wait();
     sleep(seconds);
+    std::cout << "\tend wait and delay" << std::endl;
 }
 
 int hpx_main()
@@ -27,10 +34,8 @@ int hpx_main()
     return hpx::finalize();
 }
 
-
 int main(int argc, char **argv)
 {
-
     using namespace boost::assign;
 
     std::vector<std::string> cfg;
