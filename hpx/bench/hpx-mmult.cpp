7767c35bdd15e0be39656af571b242662372f6a5
Jeremy Kemp <jakemp@uh.edu>
2015-07-16 15:32:35 -0500

    finished outlining the code, need to get it to compile

--- hpx/bench/hpx-mmult.cpp
+++ hpx/bench/hpx-mmult.cpp
@@ -2,37 +2,88 @@
 #include <hpx/runtime/threads/topology.hpp>
 #include <boost/format.hpp>
 
-#include <chrono>
+#include <matrix_block.h>
 
 using hpx::lcos::shared_future;
 using hpx::lcos::future;
 using std::vector;
 using std::cout;
 using std::endl;
-using std::chrono::high_resolution_clock;
-using time_point = std::chrono::system_clock::time_point;
 
-void print_time(time_point t1, time_point t2, std::string name) {
-     auto time = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-     cout << name << ": " << time << " microseconds" << endl;
-}
 
-struct block {
-    double *ptr;
-    int stride;
-    int row;
-    int col;
-    int width;
-    int height;
-};
+const int blocksize = 64;
 
-block rec_mult(block A, block B, block C) {
-    //termination condition
-    //sub divide
+
+block serial_mult(block A, block B, block C) {
+    for (int i = 0; i < C.width; i++) {
+        for (int j = 0; j < C.height; j++) {
+            for (int k = 0; k < A.width;k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
     return C;
 }
 
+block add_blocks(block A, block B, block result) {
+    for(int i = 0; i < A.width; i++){
+        for(int j = 0; j < A.height; j++) {
+            result[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    return result;
+}
+
+block calc_c11(block A, block B, block C) {
+    block tempC(C.block11());
+    block A11B11 = rec_mult(A.block11(), B.block11(), C.block11());
+    block A12B21 = rec_mult(A.block12(), B.block21(), tempC);
+    return add_blocks(A11B11, A12B21, C.block11());
+}
+
+block calc_c12(block A, block B, block C) {
+    block tempC(C.block12());
+    block A11B12 = rec_mult(A.block11(), B.block12(), C.block12());
+    block A12B22 = rec_mult(A.block12(), B.block22(), tempC);
+    return add_blocks(A11B12, A12B22, C.block12());
+}
+
+block calc_c21(block A, block B, block C) {
+    block tempC(C.block21());
+    block A21B11 = rec_mult(A.block21(), B.block11(), C.block21());
+    block A22B21 = rec_mult(A.block22(), B.block21(), tempC);
+    return add_blocks(A21B11, A22B21, C.block21());
+}
+
+block calc_c22(block A, block B, block C) {
+    block tempC(C.block22());
+    block A21B12 = rec_mult(A.block21(), B.block12(), C.block22());
+    block A22B22 = rec_mult(A.block22(), B.block22(), tempC);
+    return add_blocks(A21B12, A22B22, C.block22());
+}
+
+block rec_mult(block A, block B, block C) {
+    if(C.width < blocksize || C.height < blocksize ) {
+        return serial_mult(A, B, C);
+    } 
+    C11 = calc_c11(A, B, C);
+    C12 = calc_c12(A, B, C);
+    C21 = calc_c21(A, B, C);
+    C22 = calc_c22(A, B, C);
+    return C;
+}
+
+
 int hpx_main(boost::program_options::variables_map& vm) {
+    int niter = 1, N = 1000, block_size = 128;
+    srand((unsigned long)time(NULL));
+
+    block a(N*N);
+    block b(N*N);
+    block c(new double[N*N]{0}, N);
+
+    rec_mult(a, b, c);
+
     return hpx::finalize();
 }
 
---
+++ hpx/bench/matrix_block.h
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <vector>
+
+using std::vector;
+
+struct block {
+    block(block &other) : stride(other.stride), width(other.width), height(other.height), data(other.stride)
+    {
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                data[i*stride + j] = other[i][j];
+            }
+        }
+    }
+
+    block(double *matrix, int size) {
+        ptr = matrix;
+        stride = size;
+        height = size;
+        width = size;
+    }
+    block(int size) : data(size)
+    {
+        ptr = data.data();
+        const int RM = 100;
+        double rv1 = (rand() % RM) / ((double)RM);
+        double rv2 = (rand() % RM) / ((double)RM);
+        for (int i = 0; i < size; i++) {
+            data[i] = ((i+1) * rv1) / ((i+1) * rv2);
+        }
+    }
+
+    block() {}
+
+    double *ptr;
+    int stride{0};
+    int row{0};
+    int col{0};
+    int width{0};
+    int height{0};
+    vector<double> data;
+
+    double* operator[](int idx) {
+        return &(ptr[row*stride + col + idx]);
+    }
+    block block11(){
+        block tmp;
+        tmp.ptr = ptr;
+        tmp.stride = stride;
+        tmp.row = row;
+        tmp.col = col;
+        tmp.width = width/2;
+        tmp.height = height/2;
+        return tmp;
+    }
+    block block12(){
+        block tmp;
+        tmp.ptr = ptr;
+        tmp.stride = stride;
+        tmp.row = row;
+        tmp.col = col + width/2;
+        tmp.width = width - (width/2);
+        tmp.height = height/2;
+        return tmp;
+    }
+    block block21(){
+        block tmp;
+        tmp.ptr = ptr;
+        tmp.stride = stride;
+        tmp.row = row + height/2;
+        tmp.col = col;
+        tmp.width = width/2;
+        tmp.height = height - (height/2);
+        return tmp;
+    }
+    block block22(){
+        block tmp;
+        tmp.ptr = ptr;
+        tmp.stride = stride;
+        tmp.row = row + height/2;
+        tmp.col = col + width/2;
+        tmp.width = width - (width/2);
+        tmp.height = height - (height/2);
+        return tmp;
+    }
+};
