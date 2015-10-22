9344272448f09119627d384ea863f0334b5da0cd
Jeremy Kemp <jakemp@uh.edu>
2015-10-22 14:05:16 -0500

    reworking kastor jacobi code

--- omp/benchmarks/kastors-1.1/jacobi/src/poisson.c
+++ omp/benchmarks/kastors-1.1/jacobi/src/poisson.c
@@ -95,6 +95,7 @@ void check_params( struct user_parameters* params, int matrix_size,
             (*udiff)[i][j] = (*unew)[i][j] - u_exact(x, y);
             if( (*udiff)[i][j] > 1.0E-6 ) { 
                 printf("error: %d, %d: %f\n", i, j, (*udiff)[i][j]);
+            }
         }
     }
     error1 = r8mat_rms(matrix_size, matrix_size, udiff_);
