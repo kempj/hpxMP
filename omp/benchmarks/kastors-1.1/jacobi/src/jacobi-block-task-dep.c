b356c3eda94da76ea905184aa9083e9a45b560a1
Jeremy Kemp <jakemp@uh.edu>
2015-10-22 01:19:27 -0500

    reworking kastor jacobi code

--- omp/benchmarks/kastors-1.1/jacobi/src/jacobi-block-task-dep.c
+++ omp/benchmarks/kastors-1.1/jacobi/src/jacobi-block-task-dep.c
@@ -4,13 +4,11 @@
 void sweep (int nx, int ny, double dx, double dy, double *f_,
         int itold, int itnew, double *u_, double *unew_, int block_size)
 {
-    int it;
 #ifdef _OPENMP
     double (*f)[nx][ny] = (double (*)[nx][ny])f_;
     double (*u)[nx][ny] = (double (*)[nx][ny])u_;
     double (*unew)[nx][ny] = (double (*)[nx][ny])unew_;
 #endif
-    int block_x, block_y;
 
     if (block_size == 0)
         block_size = nx;
@@ -18,13 +16,13 @@ void sweep (int nx, int ny, double dx, double dy, double *f_,
     int max_blocks_x = (nx / block_size);
     int max_blocks_y = (ny / block_size);
 
-#pragma omp parallel \
-    shared(u_, unew_, f, max_blocks_x, max_blocks_y, nx, ny, dx, dy, itold, itnew, block_size) \
-    private(it, block_x, block_y)
+#pragma omp parallel
 #pragma omp single
     {
+        int it;
+        int block_x, block_y;
         for (it = itold + 1; it <= itnew; it++) {
-            // Save the current estimate.
+            /*
             for (block_x = 0; block_x < max_blocks_x; block_x++) {
                 for (block_y = 0; block_y < max_blocks_y; block_y++) {
 #pragma omp task shared(u_, unew_, block_size, nx, ny) firstprivate(block_x, block_y) \
@@ -33,7 +31,7 @@ void sweep (int nx, int ny, double dx, double dy, double *f_,
                     copy_block(nx, ny, block_x, block_y, u_, unew_, block_size);
                 }
             }
-
+            */
             // Compute a new estimate.
             for (block_x = 0; block_x < max_blocks_x; block_x++) {
                 for (block_y = 0; block_y < max_blocks_y; block_y++) {
@@ -41,16 +39,32 @@ void sweep (int nx, int ny, double dx, double dy, double *f_,
                     int xdp1 = block_x == max_blocks_x-1 ? 0 : +1;
                     int ydp1 = block_y == max_blocks_y-1 ? 0 : +1;
                     int ydm1 = block_y == 0 ? 0 : 1;
-#pragma omp task shared(u_, unew_, f_, dx, dy, nx, ny, block_size) firstprivate(block_x, block_y, xdm1, xdp1, ydp1, ydm1) \
-                    depend(out: unew[block_x * block_size: block_size][block_y * block_size: block_size]) \
-                    depend(in: f[block_x * block_size: block_size][block_y * block_size: block_size], \
-                            u[block_x * block_size: block_size][block_y * block_size: block_size], \
-                            u[(block_x - xdm1) * block_size: block_size][block_y * block_size: block_size], \
-                            u[block_x * block_size: block_size][(block_y + ydp1)* block_size: block_size], \
-                            u[block_x * block_size: block_size][(block_y - ydm1)* block_size: block_size], \
-                            u[(block_x + xdp1)* block_size: block_size][block_y * block_size: block_size])
-                    compute_estimate(block_x, block_y, u_, unew_, f_, dx, dy,
-                                     nx, ny, block_size);
+#pragma omp task shared(u_, unew_, f_, dx, dy, nx, ny, block_size) \
+                 firstprivate(block_x, block_y, xdm1, xdp1, ydp1, ydm1) \
+                 depend(inout: unew[ block_x         * block_size][ block_y         * block_size]  \
+                                  u[ block_x         * block_size][ block_y         * block_size]) \
+                 depend(in   :    f[ block_x         * block_size][ block_y         * block_size], \
+                                  u[(block_x - xdm1) * block_size][ block_y         * block_size], \
+                                  u[ block_x         * block_size][(block_y + ydp1) * block_size], \
+                                  u[ block_x         * block_size][(block_y - ydm1) * block_size], \
+                                  u[(block_x + xdp1) * block_size][ block_y         * block_size]) \
+                    { //compute_estimate(block_x, block_y, u_, unew_, f_, dx, dy, nx, ny, block_size);
+                        copy_block(nx, ny, block_x, block_y, u_, unew_, block_size);
+                        int i, j;
+                        int start_i = block_x * block_size;
+                        int start_j = block_y * block_size;
+                        for (i = start_i; i < start_i + block_size; i++) {
+                            for (j = start_j; j < start_j + block_size; j++) {
+                                if (i == 0 || j == 0 || i == nx - 1 || j == ny - 1) {
+                                    (*unew)[i][j] = (*f)[i][j];
+                                } else {
+                                    (*unew)[i][j] = 0.25 * ((*u)[i-1][j] + (*u)[i][j+1]
+                                            + (*u)[i][j-1] + (*u)[i+1][j]
+                                            + (*f)[i][j] * dx * dy);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
