e95c130c8cac9b75971a13de1caf9624189da788
Jeremy Kemp <jakemp@uh.edu>
2017-11-02 15:07:35 -0400

    changed timing reporting

--- tests/hpx/bench/micro-bench.cpp
+++ tests/hpx/bench/micro-bench.cpp
@@ -29,8 +29,8 @@ void print_time(std::vector<uint64_t> time, std::string name) {
             min = time[i];
         }
     }
-    cout << "\ntest " << name << ", average = " << total / time.size()
-         << "ns, min = " << min << ", max = " << max << endl;
+    cout << "\ntest " << name << ", average = " << (double)total / (double)time.size()
+         << "ns - min = " << min << " - max = " << max << endl;
 }
 
 void delay(int delaylength) {
@@ -41,23 +41,6 @@ void delay(int delaylength) {
         printf("%f \n", a);
 }
 
-int getdelaylengthfromtime(uint64_t delaytime) {
-    int delaylength = 0;
-    int reps = 1000;
-    boost::uint64_t starttime, lapsedtime = 0;
-    delaytime = delaytime/1.0E6; // convert from microseconds to seconds
-    delay(delaylength);
-
-    while(lapsedtime < delaytime) {
-        delaylength = delaylength * 1.1 + 1;
-        starttime = hpx::util::high_resolution_clock::now();
-        for(int i = 0; i < reps; i++) {
-            delay(delaylength);
-        }
-        lapsedtime = (hpx::util::high_resolution_clock::now() - starttime) / (double) reps;
-    }
-    return delaylength;
-}
 
 void parallel_thread(int inner_reps, int delay_length, barrier *B) {
     vector<future<void>> tasks(inner_reps);
@@ -95,7 +78,6 @@ boost::uint64_t barrier_test(int num_threads, int inner_reps, int delay_reps) {
     vector<future<void>> threads;
     barrier B(num_threads);
     for(int i = 0; i < num_threads - 1; i++) {
-        //threads.push_back(hpx::async(barrier_func, delay_length, &B));
         threads.push_back(hpx::async(barrier_func, delay_reps, &B));
     }
     B.wait();
@@ -111,7 +93,6 @@ int hpx_main(boost::program_options::variables_map& vm) {
     int reps = vm["reps"].as<int>();
     int num_threads = hpx::get_os_thread_count();
     vector<uint64_t> time(reps);
-    //int default_delay = getdelaylengthfromtime(100);//.1 microseconds
     int delay_reps = vm["delay_reps"].as<int>();
     int inner_reps = vm["inner_reps"].as<int>();
 
@@ -122,21 +103,17 @@ int hpx_main(boost::program_options::variables_map& vm) {
 
     if(test == "all" or test == "0") {
         for(int i = 0; i < reps; i++) {
-            //time[i] = par_region(num_threads, default_delay);
-            time[i] = par_region(num_threads, inner_reps, delay_reps);
+            time[i] = (par_region(num_threads, inner_reps, delay_reps))/(double)inner_reps;
         }
         print_time(time, "parallel region");
     }
     if(test == "all" or test == "1") {
         for(int i = 0; i < reps; i++) {
-            //time[i] = barrier_test(num_threads, default_delay);
-            time[i] = barrier_test(num_threads, inner_reps, delay_reps);
+            time[i] = (barrier_test(num_threads, inner_reps, delay_reps));
         }
         print_time(time, "Barrier");
     }
-
-
-    return hpx::finalize(); // Handles HPX shutdown
+    return hpx::finalize();
 }
 
 int main(int argc, char ** argv) {
