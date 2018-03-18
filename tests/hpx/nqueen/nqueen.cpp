eb09199e593a9c1e0f37799d55817cbcf889a996
Jeremy Kemp <jakemp@uh.edu>
2018-03-18 19:18:01 -0400

    reworking logic

--- tests/hpx/nqueen/nqueen.cpp
+++ tests/hpx/nqueen/nqueen.cpp
@@ -1,30 +1,118 @@
+//  Copyright (c) 2007-2017 Hartmut Kaiser, Richard D Guidry Jr.
 //  Copyright (c) 2011 Vinay C Amatya
 //  Copyright (c) 2011 Bryce Lelbach
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//  Parts of this nqueen_client.cpp has been taken from the accumulator example
+//  by Hartmut Kaiser.
 
 #include <hpx/hpx.hpp>
+#include <hpx/hpx_init.hpp>
 
-#include "server/nqueen.hpp"
+#include <boost/lexical_cast.hpp>
 
-HPX_REGISTER_COMPONENT_MODULE();
+#include <cstddef>
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
 
-typedef hpx::components::component<nqueen::server::board> board_type;
+#include "nqueen.hpp"
 
-HPX_REGISTER_COMPONENT(board_type, board);
+int hpx_main(boost::program_options::variables_map&)
+{
+    const std::size_t default_size = 8;
 
-// Serialization support for the board actions
-HPX_REGISTER_ACTION(board_type::wrapped_type::init_action, board_init_action);
+    std::size_t soln_count_total = 0;
 
-HPX_REGISTER_ACTION(board_type::wrapped_type::check_action, board_check_action);
+    hpx::naming::id_type locality_ = hpx::find_here();
 
-HPX_REGISTER_ACTION(board_type::wrapped_type::access_action,
-    board_access_action);
+    std::cout << "Enter size of board. Default size is 8." << std::endl;
+    std::cout << "Command Options: size[value] | default | print | quit"
+              << std::endl;
+    std::string cmd;
+    std::cin >> cmd;
 
-HPX_REGISTER_ACTION(board_type::wrapped_type::update_action,
-    board_update_action);
+    while (std::cin.good())
+    {
+        if(cmd == "size")
+        {
+            soln_count_total = 0;
+            std::string arg;
+            std::cin >> arg;
+            std::size_t sz = boost::lexical_cast<std::size_t>(arg);
 
-HPX_REGISTER_ACTION(board_type::wrapped_type::solve_action, board_solve_action);
+            std::size_t i = 0;
+            std::list<nqueen::board> b;
+            nqueen::board bi = hpx::new_<nqueen::board>(locality_);
+            while(i != sz)
+            {
+                b.push_back(bi);
+                ++i;
+            }
 
-HPX_REGISTER_ACTION(board_type::wrapped_type::clear_action, board_clear_action);
+            i=0;
+            for(std::list<nqueen::board>::iterator iter = b.begin();
+                iter != b.end(); ++iter)
+            {
+                iter->create(locality_);
+                iter->init_board(sz);
+                soln_count_total+= iter->solve_board(iter->access_board(),
+                                                     sz, 0, i);
+                ++i;
+            }
+            std::cout << "soln_count:" << soln_count_total << std::endl;
+            b.clear();
+        }
+        else if(cmd == "default")
+        {
+            soln_count_total = 0;
+            nqueen::board a = hpx::new_<nqueen::board>(locality_);
+            std::size_t i = 0;
+            std::vector<nqueen::board> b;
+            while(i != default_size)
+            {
+                b.push_back(a);
+                ++i;
+            }
+            i = 0;
+            for(std::vector<nqueen::board>::iterator iter = b.begin();
+                iter != b.end(); ++iter)
+            {
+                iter->create(locality_);
+                iter->init_board(default_size);
+                soln_count_total+= iter->solve_board(iter->access_board(),
+                                                     default_size, 0, i);
+                ++i;
+            }
+            std::cout << "soln_count:" << soln_count_total << std::endl;
+            b.clear();
+        }
+        else if(cmd == "print")
+        {
+            std::cout << "soln_count : " << soln_count_total << std::endl;
+        }
+        else if (cmd == "quit"){
+            //std::cout << "soln_count : " << soln_count_total << std::endl;
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid Command." << std::endl;
+            std::cout << "Options: size[value] | default | print "<<
+            "| quit" << std::endl;
+        }
+        std::cin >> cmd;
+    }
+
+    hpx::finalize();
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    return hpx::init(argc, argv);
+}
--- tests/hpx/nqueen/nqueen.hpp
+++ tests/hpx/nqueen/nqueen.hpp
@@ -4,98 +4,95 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(HPX_527D225B_F1EC_4BC5_9245_3A69C6AE5304)
-#define HPX_527D225B_F1EC_4BC5_9245_3A69C6AE5304
 
 #include <hpx/runtime.hpp>
 #include <hpx/include/client.hpp>
 
-#include <examples/nqueen/server/nqueen.hpp>
 
 #include <cstddef>
 #include <utility>
 
 namespace nqueen
 {
-    class board : public hpx::components::client_base<board, server::board>
+    typedef std::vector<std::size_t> list_type;
+
+    class board 
     {
-        typedef hpx::components::client_base<board, server::board> base_type;
+    private:
+        list_type list_;
+        std::size_t count_;
 
     public:
-        board() {}
-        board(hpx::id_type&& gid)
-          : base_type(std::move(gid))
-        {
-        }
-        board(hpx::future<hpx::id_type>&& gid)
-          : base_type(std::move(gid))
-        {
-        }
+        board() : list_(0), count_(0)
+        {}
+
+        board(list_type const& list, std::size_t, std::size_t)
+          : list_(list), count_(0)
+        {}
+
+        ~board() = default;
 
         void init_board(std::size_t size)
         {
-            return init_board_async(size).get();
-        }
+            std::size_t i = 0;
+            while(i!=size) {
+                list_.push_back(size);
+                ++i;
+             }
+         }
 
-        hpx::lcos::future<void> init_board_async(std::size_t size)
-        {
-            return hpx::async<server::board::init_action>(this->get_id(), size);
-        }
-
-        //-------------------------------------------------------
-        list_type access_board()
-        {
-            return access_board_async().get();
-        }
-
-        hpx::lcos::future<list_type> access_board_async()
-        {
-            typedef server::board::access_action action_type;
-            return hpx::async<action_type>(this->get_id());
-        }
-
-        //------------------------------------------------------
-        void update_board(std::size_t level, std::size_t pos)
-        {
-            hpx::apply<server::board::update_action>(
-                this->get_id(), level, pos);
-        }
-
-        //-----------------------------------------------------
         bool check_board(list_type const& list, std::size_t level)
         {
-            return check_board_async(list, level).get();
+            for (std::size_t i = 0; i < level; ++i) {
+                if ((list.at(i) == list.at(level)) ||
+                    (list.at(level) - list.at(i) == level - i) ||
+                    (list.at(i) - list.at(level) == level - i))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
-        hpx::lcos::future<bool> check_board_async(list_type const& list,
-            std::size_t level)
+        list_type access_board()
         {
-            typedef server::board::check_action action_type;
-            return hpx::async<action_type>(this->get_id(), list, level);
+            return list_;
         }
 
-        //---------------------------------------------------------
+        void update_board(std::size_t pos, std::size_t val)
+        {
+            list_.at(pos) = val;
+        }
+
+        void clear_board()
+        {
+            board::list_.clear();
+        }
+
         std::size_t solve_board(list_type const& list, std::size_t size,
             std::size_t level, std::size_t col)
         {
-            return solve_board_async(list, size, level, col).get();
-        }
+            board b(list, size, level);
 
-        hpx::lcos::future<std::size_t> solve_board_async(list_type const& list,
-            std::size_t size, std::size_t level, std::size_t col)
-        {
-            typedef server::board::solve_action action_type;
-            return hpx::async<action_type>(
-                this->get_id(), list, size, level, col);
-        }
-
-        //---------------------------------------------------------
-        void clear_board()
-        {
-            hpx::apply<server::board::clear_action>(this->get_id());
+            if (level == size) {
+                return 1;
+            }
+            else if (level == 0) {
+                b.update_board(level, col);
+                if (b.check_board(b.access_board(), level)) {
+                    b.count_ +=
+                        solve_board(b.access_board(), size, level + 1, col);
+                }
+            } else {
+                for (std::size_t i = 0; i < size; ++i) {
+                    b.update_board(level, i);
+                    if (b.check_board(b.access_board(), level)) {
+                        b.count_ +=
+                            solve_board(b.access_board(), size, level + 1, col);
+                    }
+                }
+            }
+            return b.count_;
         }
     };
 }
-
-#endif // HPX_527D225B_F1EC_4BC5_9245_3A69C6AE5304
-
