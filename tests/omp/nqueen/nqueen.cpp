bf147b713119ba1e21f3d1380139a96bef11e92a
Jeremy Kemp <jakemp@uh.edu>
2018-04-02 05:38:00 -0400

    starting omp version

---
+++ tests/omp/nqueen/nqueen.cpp
@@ -0,0 +1,109 @@
+//  Copyright (c) 2007-2017 Hartmut Kaiser, Richard D Guidry Jr.
+//  Copyright (c) 2011 Vinay C Amatya
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//  Parts of this nqueen_client.cpp has been taken from the accumulator example
+//  by Hartmut Kaiser.
+
+#include <hpx/hpx.hpp>
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/lcos.hpp>
+
+#include <boost/lexical_cast.hpp>
+
+#include <cstddef>
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+#include "nqueen.hpp"
+
+
+int task_create( nqueen::board sub_board, int size, int i)
+{
+    return sub_board.solve_board( sub_board.access_board(), size, 0, i);
+}
+
+
+int hpx_main(boost::program_options::variables_map&)
+{
+    const int default_size = 8;
+
+    int soln_count_total = 0;
+
+    hpx::naming::id_type locality_ = hpx::find_here();
+
+    std::cout << "Enter size of board. Default size is 8." << std::endl;
+    std::cout << "Command Options: size[value] | default | print | quit"
+              << std::endl;
+    std::string cmd;
+    std::cin >> cmd;
+
+    while (std::cin.good()) {
+        if(cmd == "size") {
+            soln_count_total = 0;
+            std::string arg;
+            std::cin >> arg;
+            int sz = boost::lexical_cast<int>(arg);
+
+            std::vector<nqueen::board> sub_boards;
+            std::vector<hpx::shared_future<int> > sub_count(sz);
+            for(int i=0; i < sz; i++) {
+                sub_boards.push_back(nqueen::board());
+                sub_boards[i].init_board(sz);
+                sub_count[i] = hpx::async(&task_create, sub_boards[i], sz, i);
+            }
+            for(int i=0; i < sz; i++) {
+                soln_count_total += sub_count[i].get();
+            }
+
+            std::cout << "soln_count:" << soln_count_total << std::endl;
+            sub_boards.clear();
+        } else if(cmd == "default") {
+            /*
+            soln_count_total = 0;
+            nqueen::board a; //= hpx::new_<nqueen::board>(locality_);
+            size_t i = 0;
+            std::vector<nqueen::board> b;
+            while(i != default_size) {
+                b.push_back(a);
+                ++i;
+            }
+            i = 0;
+            for(std::vector<nqueen::board>::iterator iter = b.begin();
+                iter != b.end(); ++iter)
+            {
+                //iter->create(locality_);
+                iter->init_board(default_size);
+                soln_count_total+= iter->solve_board(iter->access_board(),
+                                                     default_size, 0, i);
+                ++i;
+            }
+            std::cout << "soln_count:" << soln_count_total << std::endl;
+            b.clear();
+            */
+        } else if(cmd == "print") {
+            std::cout << "soln_count : " << soln_count_total << std::endl;
+        } else if (cmd == "quit") {
+            break;
+        } else {
+            std::cout << "Invalid Command." << std::endl;
+            std::cout << "Options: size[value] | default | print "<<
+            "| quit" << std::endl;
+        }
+        std::cin >> cmd;
+    }
+
+    hpx::finalize();
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    return hpx::init(argc, argv);
+}
---
+++ tests/omp/nqueen/nqueen.hpp
@@ -0,0 +1,91 @@
+//  Copyright (c) 2011 Vinay C Amatya
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <hpx/runtime.hpp>
+#include <hpx/include/client.hpp>
+
+#include <cstddef>
+#include <utility>
+
+namespace nqueen
+{
+    typedef std::vector<int> list_type;
+
+    class board 
+    {
+    private:
+        list_type list_;
+        int count_;
+
+    public:
+        board() : list_(0), count_(0)
+        {}
+
+        board(list_type const& list, int, int)
+          : list_(list), count_(0)
+        {}
+
+        ~board() = default;
+
+        void init_board(int size)
+        {
+            for(int i=0; i<size; i++) {
+                list_.push_back(size);
+            }
+        }
+
+        bool check_board(list_type const& list, int level)
+        {
+            for (int i = 0; i < level; ++i) {
+                if ((list.at(i) == list.at(level)) ||
+                    (list.at(level) - list.at(i) == level - i) ||
+                    (list.at(i) - list.at(level) == level - i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        list_type access_board()
+        {
+            return list_;
+        }
+
+        void update_board(int pos, int val)
+        {
+            list_.at(pos) = val;
+        }
+
+        void clear_board()
+        {
+            board::list_.clear();
+        }
+
+        int solve_board( list_type const& list, int size, int level, int col) 
+        {
+            board b(list, size, level);
+
+            if (level == size) {
+                return 1;
+            } else if (level == 0) {
+                b.update_board(level, col);
+                if (b.check_board(b.access_board(), level)) {
+                    b.count_ += solve_board(b.access_board(), size, level + 1, col);
+                }
+            } else {
+                for (int i = 0; i < size; ++i) {
+                    b.update_board(level, i);
+                    if (b.check_board(b.access_board(), level)) {
+                        b.count_ += solve_board(b.access_board(), size, level + 1, col);
+                    }
+                }
+            }
+            return b.count_;
+        }
+    };
+}
