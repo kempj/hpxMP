53385d5e7052ca1cbd5d9c66627a6dd4cfd996bc
Jeremy Kemp <jakemp@uh.edu>
2018-04-12 00:45:44 -0400

    simplified the hpx version

--- tests/hpx/nqueen/nqueen.cpp
+++ tests/hpx/nqueen/nqueen.cpp
@@ -36,79 +36,35 @@ int task_create( nqueen::board sub_board, int size, int i)
 }
 
 
-int hpx_main(boost::program_options::variables_map&)
+int hpx_main(int argc, char* argv[])
 {
-    const int default_size = 8;
-
     int soln_count_total = 0;
-
-    hpx::naming::id_type locality_ = hpx::find_here();
-
-    std::cout << "Enter size of board. Default size is 8." << std::endl;
-    std::cout << "Command Options: size[value] | default | print | quit"
-              << std::endl;
-    std::string cmd;
-    std::cin >> cmd;
-
-    while (std::cin.good()) {
-        if(cmd == "size") {
-            soln_count_total = 0;
-            std::string arg;
-            std::cin >> arg;
-            int sz = boost::lexical_cast<int>(arg);
-
-            std::vector<nqueen::board> sub_boards;
-            std::vector<hpx::shared_future<int> > sub_count(sz);
-            auto t1 = high_resolution_clock::now();
-            for(int i=0; i < sz; i++) {
-                sub_boards.push_back(nqueen::board());
-                sub_boards[i].init_board(sz);
-                sub_count[i] = hpx::async(&task_create, sub_boards[i], sz, i);
-            }
-            for(int i=0; i < sz; i++) {
-                soln_count_total += sub_count[i].get();
-            }
-            auto t2 = high_resolution_clock::now();
-
-            auto total = duration_cast<nanoseconds> (t2-t1).count();
-            std::cout << "time: " << total << " ns" << std::endl;
-
-            std::cout << "soln_count:" << soln_count_total << std::endl;
-            sub_boards.clear();
-        } else if(cmd == "default") {
-            /*
-            soln_count_total = 0;
-            nqueen::board a; //= hpx::new_<nqueen::board>(locality_);
-            size_t i = 0;
-            std::vector<nqueen::board> b;
-            while(i != default_size) {
-                b.push_back(a);
-                ++i;
-            }
-            i = 0;
-            for(std::vector<nqueen::board>::iterator iter = b.begin();
-                iter != b.end(); ++iter)
-            {
-                //iter->create(locality_);
-                iter->init_board(default_size);
-                soln_count_total+= iter->solve_board(iter->access_board(),
-                                                     default_size, 0, i);
-                ++i;
-            }
-            std::cout << "soln_count:" << soln_count_total << std::endl;
-            b.clear();
-            */
-        } else if(cmd == "print") {
-            std::cout << "soln_count : " << soln_count_total << std::endl;
-        } else if (cmd == "quit") {
-            break;
-        } else {
-            std::cout << "Invalid Command." << std::endl;
-            std::cout << "Options: size[value] | default | print "<<
-            "| quit" << std::endl;
-        }
-        std::cin >> cmd;
+    int sz = 8;
+    if(argc > 1) {
+        sz = atoi(argv[1]);
     }
+
+
+    std::cout << "board size: " << sz << std::endl;
+
+    std::vector<nqueen::board> sub_boards;
+    std::vector<hpx::shared_future<int> > sub_count(sz);
+    auto t1 = high_resolution_clock::now();
+    for(int i=0; i < sz; i++) {
+        sub_boards.push_back(nqueen::board());
+        sub_boards[i].init_board(sz);
+        sub_count[i] = hpx::async(&task_create, sub_boards[i], sz, i);
+    }
+    for(int i=0; i < sz; i++) {
+        soln_count_total += sub_count[i].get();
+    }
+    auto t2 = high_resolution_clock::now();
+
+    auto total = duration_cast<nanoseconds> (t2-t1).count();
+    std::cout << "time: " << total << " ns" << std::endl;
+
+    std::cout << "soln_count: " << soln_count_total << std::endl;
+    sub_boards.clear();
 
     hpx::finalize();
 
