459b940c4929e9ee5e7719bb946798af587c9695
Jeremy Kemp <jakemp@uh.edu>
2018-04-12 00:50:03 -0400

    added cleaned up version of omp

--- tests/omp/nqueen/nqueen.cpp
+++ tests/omp/nqueen/nqueen.cpp
@@ -33,61 +33,41 @@ int task_create( nqueen::board sub_board, int size, int i)
 
 int main(int argc, char* argv[]) 
 {
-    const int default_size = 8;
-
     int soln_count_total = 0;
+    int sz = 8;
+    if(argc > 1) {
+        sz = atoi(argv[1]);
+    }
 
-    std::cout << "Enter size of board. Default size is 8." << std::endl;
-    std::cout << "Command Options: size[value] | default | print | quit"
-              << std::endl;
-    std::string cmd;
-    std::cin >> cmd;
+    std::cout << "board size: " << sz << std::endl;
+
 #pragma omp parallel
 {
 #pragma omp single
 {
-    while (std::cin.good()) {
-        int sz = default_size;
-        if(cmd == "size") {
-            soln_count_total = 0;
-            std::string arg;
-            std::cin >> arg;
-            sz = atoi(arg.c_str());
+    nqueen::board *sub_boards = new nqueen::board[sz];
+    int *sub_count = new int[sz];
+    auto t1 = high_resolution_clock::now();
+    for(int i=0; i < sz; i++) {
+        sub_boards[i] = nqueen::board();
+        sub_boards[i].init_board(sz);
+        #pragma omp task 
+        {
+            sub_count[i] = task_create( sub_boards[i], sz, i);
         }
-
-        nqueen::board *sub_boards = new nqueen::board[sz];
-        int *sub_count = new int[sz];
-        auto t1 = high_resolution_clock::now();
-        for(int i=0; i < sz; i++) {
-            sub_boards[i] = nqueen::board();
-            sub_boards[i].init_board(sz);
-            #pragma omp task 
-            {
-                sub_count[i] = task_create( sub_boards[i], sz, i);
-            }
-        }
-        #pragma omp taskwait
-        for(int i=0; i < sz; i++) {
-            soln_count_total += sub_count[i];
-        }
-        auto t2 = high_resolution_clock::now();
-
-        auto total = duration_cast<nanoseconds> (t2-t1).count();
-        std::cout << "time: " << total << " ns" << std::endl;
-        std::cout << "soln_count:" << soln_count_total << std::endl;
-        delete[] sub_boards;
-        delete[] sub_count;
-
-        if(cmd == "print") {
-            std::cout << "soln_count : " << soln_count_total << std::endl;
-        } else if (cmd == "quit") {
-            break;
-        } else {
-            std::cout << "Options: size[value] | default | print "<<
-            "| quit" << std::endl;
-        }
-        std::cin >> cmd;
     }
+    #pragma omp taskwait
+    for(int i=0; i < sz; i++) {
+        soln_count_total += sub_count[i];
+    }
+    auto t2 = high_resolution_clock::now();
+
+    auto total = duration_cast<nanoseconds> (t2-t1).count();
+    std::cout << "time: " << total << " ns" << std::endl;
+    std::cout << "soln_count:" << soln_count_total << std::endl;
+    delete[] sub_boards;
+    delete[] sub_count;
+
 }}//end parallel
     return 0;
 }
